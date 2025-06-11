ff2308a9d670ac3db4d85fb5d546ab377240f6a4
shadrack <90805297+sh6drack@users.noreply.github.com>
2025-06-11 06:49:26 -0400

    live

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [isLive, setIsLive] = useState(false)
-
-  return (    
-    <div className="app"></div>
-
-    )
+  return <div style={{ width: '100%', height: '100%', background: '#000' }} />
 }
 
 export default App
